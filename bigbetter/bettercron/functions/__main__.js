7151748f4b7df991634f41238b46fc1b5c400d89
Jerry Yuan Chen <jerrych.97@gmail.com>
2018-02-03 22:11:43 -0500

    lol

---
+++ bigbetter/bettercron/functions/__main__.js
@@ -0,0 +1,12 @@
+const lib = require('lib')({token: process.env.STDLIB_TOKEN});
+
+/**
+* A basic Hello World function
+* @param {string} name Who you're saying hello to
+* @returns {string}
+*/
+module.exports = (context, callback) => {
+  // lib.bigbetter.betterdb['@dev'].getgroup(channel, (err, value) => {
+    
+  // }
+};
